8d11654ef310d8968aad37e4a91294700e1bf215
Leon Linhart <themrmilchmann@gmail.com>
2023-04-14 19:34:36 +0200

    build: use Java 8 to run tests by default

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import io.github.themrmilchmann.gradle.toolchainswitches.ExperimentalToolchainSwitchesApi
+import io.github.themrmilchmann.gradle.toolchainswitches.inferLauncher
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.*
@@ -100,24 +102,10 @@ tasks {
     withType<Test>().configureEach {
         useJUnitPlatform()
 
-        /*
-         * This does not work because there is no way for the plugin to respect
-         * this convention while keeping the configuration lazy. In practice,
-         * this means that the task defaults to the project-wide toolchain while
-         * it should default to a toolchain for Java 8.
-         *
-         * We work around this by exposing a few experimental functions that can
-         * be used to configure tools (taking into account properties) starting
-         * in 0.3.0.
-         *
-         * TODO Adapt this task to use 0.3.0's toolchain configuration
-         *      functions.
-         *
-         * See https://github.com/gradle/gradle/issues/14768
-         */
-//        javaLauncher.convention(project.javaToolchains.launcherFor {
-//            languageVersion.set(JavaLanguageVersion.of(8))
-//        })
+        @OptIn(ExperimentalToolchainSwitchesApi::class)
+        javaLauncher.set(inferLauncher(default = project.javaToolchains.launcherFor {
+            languageVersion.set(JavaLanguageVersion.of(8))
+        }))
 
         systemProperty("junit.jupiter.execution.parallel.enabled", true)
         systemProperty("junit.jupiter.execution.parallel.mode.default", "concurrent")
